bdbd8287b1c4d638a035c062b2cf524cae236a0b
Thomas REMY <contact.t.remy777@gmail.com>
2022-11-25 18:01:53 +0100

    Modified main

--- src/main/java/fr/cyu/depinfo/Main.java
+++ src/main/java/fr/cyu/depinfo/Main.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        File zipFile = new File("src/main/resources/Modele_odt_projet.odt");
+        File zipFile = new File("src/main/resources/Test.odt");
         File outDir = new File("src/main/resources/out");
         try {
             ZipManager.unzip(zipFile, outDir);
@@ -16,7 +16,7 @@ public class Main {
             e.printStackTrace();
         }
         File metaXML = new File("src/main/resources/out/meta.xml");
-        File newMetaXML = new File("src/main/resources/out/out.xml");
+        File newMetaXML = new File("src/main/resources/out/meta.xml");
         ParsedFile parsedMeta = new ParsedFile(metaXML);
         MetadataExtractor meta = new MetadataExtractor(parsedMeta);
         System.out.println(meta.getTitle());
